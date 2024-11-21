2fe57516c8a9d7f7a9c8e29e3ffdbc054ee8f25e
syuan0303 <a112223014@mail.shu.edu.tw>
2024-11-22 07:02:46 +0800

    113-11-15

--- 113-11-15/inheritance5/src/RPG.java
+++ 113-11-15/inheritance5/src/RPG.java
@@ -5,30 +5,40 @@ public class RPG {
     }
 
     static void demoSwordsMan() {
-        var swordsMan = new SwordsMan();
+        SwordsMan swordsMan = new SwordsMan();
         swordsMan.setName("Justin");
         swordsMan.setLevel(1);
         swordsMan.setBlood(200);
-       // System.out.printf("劍士：(%s, %d, %d)%n", swordsMan.getName(),
-    //            swordsMan.getLevel(), swordsMan.getBlood());
-        System.out.println(SwordsMan);
-        swordsMan.fight();
+//        System.out.printf("劍士：(%s, %d, %d)%n", swordsMan.getName(),
+//                swordsMan.getLevel(), swordsMan.getBlood());
+        System.out.println(swordsMan);
+//        swordsMan.fight();
+        drawFight(swordsMan);
     }
 
     static void demoMagician() {
-        var magician = new Magician();
+        Magician magician = new Magician();
         magician.setName("Monica");
         magician.setLevel(1);
         magician.setBlood(100);
-   //     System.out.printf("魔法師：(%s, %d, %d)%n", magician.getName(),
-   //             magician.getLevel(), magician.getBlood());
+//        System.out.printf("魔法師：(%s, %d, %d)%n", magician.getName(),
+//                magician.getLevel(), magician.getBlood());
         System.out.println(magician);
-     //   magician.fight();
-     //   magician.cure();
+//        magician.fight();
+//        magician.cure();
         drawFight(magician);
     }
-}
 
-static void drawFight(Role role){
-    if(role instanceof SwordsMan)
+    static void drawFight(Role role) {
+        if (role instanceof SwordsMan) {
+            System.out.print(role.getName());
+            role.fight();
+        } else if (role instanceof Magician) {
+            System.out.print(role.getName());
+            role.fight();
+            ((Magician)role).cure();
+        } else {
+            System.out.println("此角色不存在");
+        }
+    }
 }
